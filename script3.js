198540e7fb0411e5e29fdfc19f8a05a5ef00b803
Uday243436 <RUdayKumar@thestackly.com>
2026-04-29 12:05:49 +0530

    COMMIT

---
+++ script3.js
@@ -0,0 +1,6 @@
+function studentForm(name, age, course) {
+    console.log("Name: " + name);
+    console.log("Age: " + age);
+    console.log("Course: " + course);
+}
+studentForm("Naveen", 22, "MERN");
